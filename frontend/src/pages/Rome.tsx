5c41a18de8471023e5cbbdb568b747472518cb94
leakyhose <yimingsu2007@gmail.com>
2026-01-17 13:28:49 -0800

    Set 1453 as max value year

--- frontend/src/pages/Rome.tsx
+++ frontend/src/pages/Rome.tsx
@@ -6,8 +6,8 @@ export default function Rome() {
   const [year, setYear] = useState(2)
 
   return (
-    <div className="relative w-screen h-screen overflow-hidden">
-      <YearSlider onChange={setYear} initialValue={2} min={2} max={2000} />
+    <div className="relative w-screen h-screen overflow-hidden bg-black">
+      <YearSlider onChange={setYear} initialValue={2} min={2} max={1453} />
       <MapCanvas year={year} />
     </div>
   )
