3037c4f7632cad208bc89b3568381e9767c47f2c
leakyhose <yimingsu2007@gmail.com>
2026-01-17 12:59:55 -0800

    Add slider to rome page

--- frontend/src/pages/Rome.tsx
+++ frontend/src/pages/Rome.tsx
@@ -1,9 +1,14 @@
+import { useState } from 'react'
 import MapCanvas from '@src/components/MapCanvas'
+import YearSlider from '@src/components/YearSlider'
 
 export default function Rome() {
+  const [year, setYear] = useState(2)
+
   return (
-    <div className="w-screen h-screen overflow-hidden">
-      <MapCanvas />
+    <div className="relative w-screen h-screen overflow-hidden">
+      <YearSlider onChange={setYear} initialValue={2} min={2} max={2000} />
+      <MapCanvas year={year} />
     </div>
   )
 }
