64c4fe1a5b4ca2c61ddb337d3626bb24dd117cca
leakyhose <yimingsu2007@gmail.com>
2026-01-17 16:08:53 -0800

    Clean up comments in backend

--- backend/main.py
+++ backend/main.py
@@ -4,10 +4,8 @@ from api.rome import router as rome_router
 
 app = FastAPI()
 
-# Mount static files directory
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# Include API routers
 app.include_router(rome_router)
 
 @app.get("/")
